06c739d2be8454030a547e61490b589428cfafa5
datagutten <datagutten@datagutten.net>
2021-10-30 21:21:58 +0200

    Skip test on imma.gr error

--- tests/imma_grTest.php
+++ tests/imma_grTest.php
@@ -1,4 +1,6 @@
 <?php
+
+use datagutten\image_host\exceptions\UploadFailed;
 use PHPUnit\Framework\TestCase;
 use datagutten\image_host\imma_gr;
 
@@ -7,7 +9,14 @@ final class imma_grTest extends TestCase
     public function testUpload()
     {
         $host = new imma_gr();
-        $upload = $host->upload(__DIR__.'/1024px-PHP-logo.svg.png');
-        $this->assertStringContainsString('https://imma.gr/', $upload);
+		try
+		{
+			$upload = $host->upload(__DIR__ . '/1024px-PHP-logo.svg.png');
+		}
+		catch (UploadFailed $e)
+		{
+			$this->markTestSkipped($e->getMessage());
+		}
+		$this->assertStringContainsString('https://imma.gr/', $upload);
     }
 }
